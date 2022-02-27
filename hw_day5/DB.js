b3d56fe1133a990352fae2aef1fabf51b0b082f4
avo888 <avoevodin8888@gmail.com>
2022-02-27 22:07:22 +0300

    added uuid for the posts

--- hw_day5/DB.js
+++ hw_day5/DB.js
@@ -1,15 +1,18 @@
 const d1 = new Date()
 const d2 = new Date()
+const { v4: uuidv4 } = require('uuid')
 
 const db = {
   posts: [
     {
+      id: uuidv4(),
       date: `${d1.toDateString()} ${d1.toLocaleTimeString()}`,
       username: 'John',
       message: 'Hi there!',
       image: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
     },
     {
+      id: uuidv4(),
       date: `${d2.toDateString()} ${d2.toLocaleTimeString()}`,
       username: 'Mike',
       message: 'Whats up!',
