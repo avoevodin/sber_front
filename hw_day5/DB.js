91ee66a09383725bb45aa2bcdd6a9f70edfa616b
avo888 <avoevodin8888@gmail.com>
2022-02-27 21:21:42 +0300

    added completed task

---
+++ hw_day5/DB.js
@@ -0,0 +1,23 @@
+const d1 = new Date()
+const d2 = new Date()
+
+const db = {
+  posts: [
+    {
+      date: `${d1.toDateString()} ${d1.toLocaleTimeString()}`,
+      username: 'John',
+      message: 'Hi there!',
+      image: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+    },
+    {
+      date: `${d2.toDateString()} ${d2.toLocaleTimeString()}`,
+      username: 'Mike',
+      message: 'Whats up!',
+      image: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg',
+    },
+  ],
+}
+
+module.exports = {
+  db,
+}
--- hw_day5/app.js
+++ hw_day5/app.js
@@ -1,6 +1,6 @@
-const { response } = require('express')
 const express = require('express')
 const path = require('path')
+const { db } = require('./DB')
 
 const PORT = 3000
 
@@ -9,8 +9,29 @@ const app = express()
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'src', 'views'))
 
+// this setting allows express to receive form data
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.render('main')
+  const usersQuery = req.query
+  let postsForRender = db.posts
+  if (usersQuery.reverse === 'true') {
+    postsForRender = postsForRender.slice().reverse()
+  }
+  if (usersQuery.limit !== undefined && Number.isNaN(+usersQuery.limit) === false) {
+    postsForRender = postsForRender.slice(0, usersQuery.limit)
+  }
+  res.render('main', { listOfPosts: postsForRender })
+})
+
+app.post('/addpost', (req, res) => {
+  const formData = req.body
+  const currentDate = new Date()
+
+  formData.date = `${currentDate.toDateString()} ${currentDate.toLocaleTimeString()}`
+  db.posts.push(formData)
+
+  res.redirect('/')
 })
 
 app.get('*', (req, res) => {
