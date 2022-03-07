e0e7a303b1d8abe7d4f4cda7bb51e57ae071c58f
avo888 <avoevodin8888@gmail.com>
2022-03-07 21:47:59 +0300

    added header and main layout added init server config

--- auth_cookies_example/simple_sessions/.eslintrc.js
+++ auth_cookies_example/simple_sessions/.eslintrc.js
@@ -12,5 +12,7 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    semi: ['error', 'never'],
+    'no-console': 0,
   },
-};
+}
--- auth_cookies_example/simple_sessions/app.js
+++ auth_cookies_example/simple_sessions/app.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
 
 const server = express()
 const PORT = process.env.PORT || 3000
+
+server.set('view engine', 'hbs')
+server.set('views', path.join(process.env.PWD, 'src', 'views'))
+hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'))
+
+server.get('/', (req, res) => {
+  res.render('main')
+})
 
 server.listen(PORT, () => {
   console.log(`The server has been started on port ${PORT}`)
