417073dc28318c9191b160568e62df7d298e0f79
avo888 <avoevodin8888@gmail.com>
2022-03-13 20:03:52 +0300

    added main component added todo list component

--- day_10_examples/src/App.js
+++ day_10_examples/src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header';
 import TodoListProvider from './contexts/TodoListContext';
 import './App.css';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <TodoListProvider>
       <div className="container py-5">
         <Header />
+        <Main />
       </div>
     </TodoListProvider >
   );
