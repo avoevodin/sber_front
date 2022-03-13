0e28ec1ba78daa1e76dfc0e1f73a8721b40eae66
avo888 <avoevodin8888@gmail.com>
2022-03-13 19:15:37 +0300

    added todos context using

--- day_10_examples/src/App.js
+++ day_10_examples/src/App.js
@@ -1,10 +1,14 @@
+import Header from './components/Header/Header';
+import TodoListProvider from '../contexts/TodoListContext';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      empty
-    </div>
+    <TodoListProvider>
+      <div className="container py-5">
+        <Header />
+      </div>
+    </TodoListProvider >
   );
 }
 
