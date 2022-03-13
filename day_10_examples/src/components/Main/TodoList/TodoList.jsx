dba0033383c1d6d6e4a2768c99660e2864d409fd
avo888 <avoevodin8888@gmail.com>
2022-03-13 20:18:00 +0300

    added todo item component logic

--- day_10_examples/src/components/Main/TodoList/TodoList.jsx
+++ day_10_examples/src/components/Main/TodoList/TodoList.jsx
@@ -1,5 +1,17 @@
+import { useContext } from "react"
+import { TodoListContext } from "../../../contexts/TodoListContext"
+import TodoItem from "./TodoItem/TodoItem"
+
 const TodoList = () => {
-    return null
+    const { todos } = useContext(TodoListContext)
+
+    return (
+        <ul class="list-group">
+            {todos.map((todo, i) => {
+                <TodoItem id={todo.id} index={i} text={todo.text} completed={todo.completed} />
+            })}
+        </ul>
+    )
 }
 
 export default TodoList
