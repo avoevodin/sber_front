f513851b49dce89d35727accb5d67e20ee556fd4
avo888 <avoevodin8888@gmail.com>
2022-03-13 20:21:41 +0300

    added todo item component logic

--- day_10_examples/src/components/Main/TodoList/TodoItem/TodoItem.jsx
+++ day_10_examples/src/components/Main/TodoList/TodoItem/TodoItem.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react"
+import { TodoListContext } from "../../../../contexts/TodoListContext"
+
 const TodoItem = (id, text, index, completed) => {
+    const { completeTodo, deleteTodo } = useContext(TodoListContext)
+    const deleteHandler = () => deleteTodo(id)
+    const completeHandler = () => deleteTodo(completed)
     return (
         <li className="list-group-item">
             <span>
                 {index + 1}. {text}
             </span>
             <div>
-                <button type="button" class="btn btn-success">Success</button>
-                <button type="button" class="btn btn-danger">Danger</button>
+                <button onClick={completeHandler} type="button" class="btn btn-success">Success</button>
+                <button onClick={deleteHandler} type="button" class="btn btn-danger">Danger</button>
             </div>
         </li>
     )
