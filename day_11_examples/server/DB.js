67a3cc6806b87f0d1324e1d4950b9405abd9024b
avo888 <avoevodin8888@gmail.com>
2022-03-17 23:27:28 +0300

    added phones api

---
+++ day_11_examples/server/DB.js
@@ -0,0 +1,32 @@
+const db = {
+    phones: [
+        {
+            id: 1,
+            phone: '+7-(999)-888-88-88',
+            name: 'Name1',
+            email: 'name1@example.com',
+        },
+        {
+            id: 2,
+            phone: '+7-(999)-888-88-77',
+            name: 'Name2',
+            email: 'name2@example.com',
+        },
+        {
+            id: 3,
+            phone: '+7-(999)-888-88-66',
+            name: 'Name3',
+            email: 'name3@example.com',
+        },
+        {
+            id: 4,
+            phone: '+7-(999)-888-88-44',
+            name: 'Name4',
+            email: 'name4@example.com',
+        },
+    ]
+}
+
+module.exports = {
+    db
+}
---
+++ day_11_examples/server/app.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const { db } = require('./DB')
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/v1/phones', (req, res) => {
+    const dataForClient = db.phones.map(({ email, ...rest }) => rest)
+    res.json(dataForClient)
+})
+
+app.get('/api/v1/phones/:id', (req, res) => {
+    const { id } = req.params
+    const dataForClient = db.phones.find((phone) => phone.id === +id)
+
+    if (dataForClient) {
+        return res.json(dataForClient)
+    }
+    return res.sendStatus(404)
+})
+
+app.post('/api/v1/phones/', (req, res) => {
+    const dataFromClient = req.body
+
+    const newPhone = {
+        ...dataFromClient,
+        id: Date.now().toString(),
+    }
+
+    if (dataFromClient) {
+        db.phones.push(newPhone)
+    }
+
+    return res.json(newPhone)
+})
+
+app.delete('/api/v1/phones/:id', (req, res) => {
+    const { id } = req.params
+    const index = db.phones.findIndex((phone) => phone.id === +id)
+
+    if (index > -1) {
+        db.phones.splice(index, 1)
+        return res.sendStatus(200)
+    }
+    return res.sendStatus(404)
+})
+
+app.listen(PORT, () => {
+    console.log(`The server has been started on port ${PORT}`)
+})
+
+module.exports = {
+    db
+}
