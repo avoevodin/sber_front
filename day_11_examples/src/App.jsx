e9d8ba9a8dbf5c4401a4f16be087f48785534902
avo888 <avoevodin8888@gmail.com>
2022-03-17 22:00:45 +0300

    added example of react router nav bar

--- day_11_examples/src/App.jsx
+++ day_11_examples/src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+import Main from './components/Main/Main'
+import About from './components/About/About'
+import Header from './components/Header/Header'
+import Info from './components/Info/Info'
 
 function App() {
   return (
-    <div className="container py-5">
-      hi
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="container py-5">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/info" element={<Info />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ day_11_examples/src/components/About/About.jsx
@@ -0,0 +1,7 @@
+function About() {
+  return (
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, incidunt.</p>
+  )
+}
+
+export default About
