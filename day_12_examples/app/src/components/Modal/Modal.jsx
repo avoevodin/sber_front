46c317e42f9866069eb36a02b6a83d924b8e9974
avo888 <avoevodin8888@gmail.com>
2022-03-21 03:21:37 +0300

    updated inner modal with moving to madal component file

--- day_12_examples/app/src/components/Modal/Modal.jsx
+++ day_12_examples/app/src/components/Modal/Modal.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom'
-import ModalInner from './ModalInner'
+import { useEffect } from 'react'
+import styles from './modal.module.css'
 
 function Modal({ children, state, ...rest }) {
   return ReactDOM.createPortal(
@@ -10,6 +11,27 @@ function Modal({ children, state, ...rest }) {
     </ModalInner>
     ),
     document.getElementById('modal-root'),
+  )
+}
+
+function ModalInner({ children, onClose }) {
+  const escHandler = (e) => {
+    if (e.code === 'Escape') {
+      onClose()
+    }
+  }
+
+  useEffect(() => {
+    window.document.addEventListener('keydown', escHandler)
+    return () => window.document.removeEventListener('keydown', escHandler)
+  }, [])
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.inner}>
+        {children}
+      </div>
+    </div>
   )
 }
 
