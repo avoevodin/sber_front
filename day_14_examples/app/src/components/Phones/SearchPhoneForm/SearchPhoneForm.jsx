8e6a9ca5a7931e3f09c8a6cd16265cce217a027b
avo888 <avoevodin8888@gmail.com>
2022-03-25 22:27:31 +0300

    added search form init fetch

--- day_14_examples/app/src/components/Phones/SearchPhoneForm/SearchPhoneForm.jsx
+++ day_14_examples/app/src/components/Phones/SearchPhoneForm/SearchPhoneForm.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useRef, useState } from 'react'
+
+// const isMount = false
+
 const SearchPhoneForm = () => {
-  console.log('')
+  const [searchInput, setSearchInput] = useState('')
+  const isMount = useRef(false)
+
+  useEffect(() => {
+    if (isMount.current) {
+      const filter = {
+        search: searchInput,
+      }
+      const preparedFilterForURL = encodeURIComponent(JSON.stringify(filter))
+      fetch(`http://localhost:3000/api/v1/phones/?filter=${preparedFilterForURL}`)
+        .then((response) => response.json())
+        .then(console.log)
+    } else {
+      isMount.current = true
+    }
+  }, [searchInput])
+
+  const changeHandler = (e) => {
+    setSearchInput(e.target.value)
+  }
+
   return (
     <form className="d-flex flex-column align-items-center">
       <div className="mb-3">
-        <input name="name" placeholder="name" type="text" className="form-control" />
+        <input
+          name="name"
+          placeholder="name"
+          type="text"
+          className="form-control"
+          value={searchInput}
+          onChange={changeHandler}
+        />
       </div>
     </form>
   )
