e8cb32cc4d5b869c5988f869d9868448db2cc2c4
avo888 <avoevodin8888@gmail.com>
2022-03-25 19:16:58 +0300

    added phone list animation

--- day_13_examples/app/src/components/Phones/PhonesItem/PhonesItem.jsx
+++ day_13_examples/app/src/components/Phones/PhonesItem/PhonesItem.jsx
@@ -1,15 +1,31 @@
+import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 
-const PhonesItem = ({ name, phone, id }) => (
-  <Link className="list-group-item list-group-item-action" to={`/phones/${id}`}>
-    <span className="pe-4">
-      {name}
-    </span>
-    <span>
+const phonesItemVariants = {
+  start: {
+    scaleY: 0,
+    opacity: 0,
+    zIndex: -1,
+  },
+  end: {
+    scaleY: 1,
+    opacity: 1,
+    zIndex: 1,
+  },
+}
 
-      {phone}
-    </span>
-  </Link>
+const PhonesItem = ({ name, phone, id }) => (
+  <motion.div variants={phonesItemVariants}>
+    <Link className="list-group-item list-group-item-action" to={`/phones/${id}`}>
+      <span className="pe-4">
+        {name}
+      </span>
+      <span>
+
+        {phone}
+      </span>
+    </Link>
+  </motion.div>
 )
 
 export default PhonesItem
--- day_13_examples/app/src/components/Phones/PhonesList/PhonesList.jsx
+++ day_13_examples/app/src/components/Phones/PhonesList/PhonesList.jsx
@@ -1,16 +1,33 @@
+import { motion } from 'framer-motion'
 import { useContext } from 'react'
 import { PhonesContext } from '../Phones'
 import PhonesItem from '../PhonesItem/PhonesItem'
+
+const phonesListVariants = {
+  start: {
+    opacity: 0,
+  },
+  end: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+}
 
 const PhonesList = () => {
   const { phones } = useContext(PhonesContext)
   return (
     <div className="d-flex justify-content-center">
-      <div className="list-group">
-        {phones.map((phone) => (
-          <PhonesItem key={phone.id} {...phone} />
-        ))}
-      </div>
+      {
+        phones.length ? (
+          <motion.div variants={phonesListVariants} initial="start" animate="end" className="list-group">
+            {phones.map((phone) => (
+              <PhonesItem key={phone.id} {...phone} />
+            ))}
+          </motion.div>
+        ) : null
+      }
     </div>
   )
 }
