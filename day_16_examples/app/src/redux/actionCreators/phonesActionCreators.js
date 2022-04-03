2b2b7571af9f117838f4230d2465d82658a6771f
avo888 <avoevodin8888@gmail.com>
2022-04-03 21:13:29 +0300

    added acs for phones project

--- day_16_examples/app/src/redux/actionCreators/phonesActionCreators.js
+++ day_16_examples/app/src/redux/actionCreators/phonesActionCreators.js
@@ -1,12 +1,49 @@
-import { SET_PHONES } from '../types/phonesTypes'
+import { DELETE_PHONE, SET_PHONES, UPDATE_PHONE } from '../types/phonesTypes'
 
-const setPhones = (payload) => ({
+const setPhones = (newPhones) => ({
   type: SET_PHONES,
-  payload,
+  payload: newPhones,
 })
 
-const setPhonesQuery = () => async (dispatch) => {
+export const setPhonesQuery = () => async (dispatch) => {
   const response = await fetch('http://localhost:3000/api/v1/phones/')
   const dataFromServer = await response.json()
   dispatch(setPhones(dataFromServer))
+}
+
+const deletePhone = (id) => ({
+  type: DELETE_PHONE,
+  payload: id,
+})
+
+export const deletePhoneQuery = (id) => async (dispatch) => {
+  const response = await fetch(`http://localhost:3000/api/v1/phones/${id}`, {
+    method: 'DELETE',
+  })
+  if (response.status === 200) {
+    dispatch(deletePhone(id))
+  }
+}
+
+const updatePhone = (newPhone) => ({
+  type: UPDATE_PHONE,
+  payload: newPhone,
+})
+
+export const updatePhoneQuery = (id, formData) => async (dispatch) => {
+    const response = await fetch(`http://localhost:3000/api/v1/phones/${ .id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+
+    if (response.status === 200) {
+      const updatedPhoneFromServer = await response.json()
+      setPhone(updatedPhoneFromServer)
+      dispatch(updatePhone(updatedPhoneFromServer ))
+    } else {
+      alert('Wrong data')
+    } 
 }
