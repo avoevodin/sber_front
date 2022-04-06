6850f7c023946d2b58841eb2ab4d9562860aa830
avo888 <avoevodin8888@gmail.com>
2022-04-06 22:55:53 +0300

    added posts list layout

--- day_17_examples/app/src/components/PostsList/PostsList.jsx
+++ day_17_examples/app/src/components/PostsList/PostsList.jsx
@@ -1,8 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { loadAllPosts } from '../../redux/actions/postAC'
+import PostItem from '../PostItem/PostItem'
+
 const PostsList = () => {
-  console.log('hi')
+  const dispatch = useDispatch()
+  const posts = useSelector((store) => store.posts)
+
+  useEffect(() => {
+    dispatch(loadAllPosts())
+  }, [])
+
+  if (!posts.length) return (<div>Posts list is empty</div>)
+
   return (
     <div>
-      list
+      {posts.map((post) => <PostItem key={post._id} {...post} />)}
     </div>
   )
 }
--- day_17_examples/app/src/redux/actions/postAC.js
+++ day_17_examples/app/src/redux/actions/postAC.js
@@ -2,11 +2,11 @@ import { API_TOKEN } from '../../constans'
 import { SET_ALL_POSTS } from '../types/postTypes'
 
 const setPosts = (payload) => ({
-  action: SET_ALL_POSTS,
+  type: SET_ALL_POSTS,
   payload,
 })
 
-export const setAllPosts = () => async (dispatch) => {
+export const loadAllPosts = () => async (dispatch) => {
   const res = await fetch('https://api.react-learning.ru/posts', {
     headers: {
       authorization: `Bearer ${API_TOKEN}`,
