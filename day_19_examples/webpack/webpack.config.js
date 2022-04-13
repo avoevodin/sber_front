14e1c547f87327cfd3a3b7405252e6cbdbb73f08
avo888 <avoevodin8888@gmail.com>
2022-04-14 02:26:39 +0300

    added css for webpack

--- day_19_examples/webpack/webpack.config.js
+++ day_19_examples/webpack/webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 let mode = 'development'
 
 if (process.env.NODE_ENV === "production") {
@@ -6,4 +8,19 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
     mode,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {publicPath: ""}
+                    },
+                    "css-loader"
+                ],
+            }
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()],
 }
