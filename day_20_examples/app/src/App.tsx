b2e91acbbd9c35f7cace524713f599ef29e439c0
avo888 <avoevodin8888@gmail.com>
2022-04-15 20:32:42 +0300

    added todos init

--- day_20_examples/app/src/App.tsx
+++ day_20_examples/app/src/App.tsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import Todo from './types/todo.types'
+
+// const initTodosState: Todo[] = [{
+//   id: Date.now(),
+//   title: 'First todos',
+//   completed: false,
+// }]]
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const [todos, setTodos] = useState<Todo[]>([{
+    id: Date.now(),
+    title: 'First todos',
+    completed: false,
+  }])
+  console.log(setTodos)
   return (
     <div className="App">
       <Header title="Title in header" version={1} />
@@ -18,6 +31,22 @@ const App = () => {
       >
         Change counter
       </button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          Title:
+          {' '}
+          {todo.title}
+          <br />
+          Completed:
+          {' '}
+          {todo.completed ? 'done' : 'undone'}
+        </div>
+      ))}
     </div>
   )
 }
