bcaae11cd09bdaf4ee5d24244c341182b860a893
avo888 <avoevodin8888@gmail.com>
2022-04-15 20:20:00 +0300

    added change counter button

--- day_20_examples/app/src/App.tsx
+++ day_20_examples/app/src/App.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 
-const App = () => (
-  <div className="App">
-    <Header title="Title in header" version={1} />
-  </div>
-)
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  return (
+    <div className="App">
+      <Header title="Title in header" version={1} />
+      Counter:
+      {' '}
+      {counter}
+      <br />
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => setCounter((prev) => prev + 1)}
+      >
+        Change counter
+      </button>
+    </div>
+  )
+}
 
 export default App
