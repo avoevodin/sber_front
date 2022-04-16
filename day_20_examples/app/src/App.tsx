6cbd262d1f143178028e4c7b71412b78072ebc01
avo888 <avoevodin8888@gmail.com>
2022-04-16 17:05:59 +0300

    added text for todos

--- day_20_examples/app/src/App.tsx
+++ day_20_examples/app/src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
@@ -11,12 +13,24 @@ import Todo from './types/todo.types'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
-  const [todos, setTodos] = useState<Todo[]>([{
-    id: Date.now(),
-    title: 'First todos',
-    completed: false,
-  }])
-  console.log(setTodos)
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      id: 'dkfajdsfkajsdfdsaf',
+      title: 'First todos',
+      completed: false,
+      text: 'Text for first todo',
+    },
+    {
+      id: Date.now(),
+      title: 'Second todos',
+      completed: true,
+    },
+  ])
+
+  const deleteHandler = (id: Todo['id']) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
   return (
     <div className="App">
       <Header title="Title in header" version={1} />
@@ -37,7 +51,7 @@ const App = () => {
       <br />
       <br />
       {todos.map((todo) => (
-        <div key={todo.id}>
+        <div key={todo.id} onClick={() => deleteHandler(todo.id)}>
           Title:
           {' '}
           {todo.title}
@@ -45,6 +59,16 @@ const App = () => {
           Completed:
           {' '}
           {todo.completed ? 'done' : 'undone'}
+          {todo.text && (
+            <>
+              <br />
+              Text:
+              {' '}
+              {todo.text}
+            </>
+          )}
+          <br />
+          <br />
         </div>
       ))}
     </div>
---
+++ day_20_examples/app/src/types/todo.types.ts
@@ -0,0 +1,6 @@
+export default interface Todo {
+    id: string | number;
+    title: string;
+    completed: boolean;
+    text?: string;
+}
